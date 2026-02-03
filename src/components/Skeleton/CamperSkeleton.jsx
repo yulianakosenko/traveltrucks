94350453b04db9c0ef27e9a57b063a09c9b0f701
YULIYA_KOSTENKO <yuliashkodina1@gmail.com>
2026-02-03 11:31:46 +0100

    Fix header layout and align catalog spacing to design

--- src/components/Skeleton/CamperSkeleton.jsx
+++ src/components/Skeleton/CamperSkeleton.jsx
@@ -1,12 +1,12 @@
-export default function CamperSkeleton() {
-  return (
-    <div
-      style={{
-        height: 180,
-        background: "#F2F4F7",
-        borderRadius: 12,
-        marginBottom: 16,
-      }}
-    />
-  );
-}
+// export default function CamperSkeleton() {
+//   return (
+//     <div
+//       style={{
+//         height: 180,
+//         background: "#F2F4F7",
+//         borderRadius: 12,
+//         marginBottom: 16,
+//       }}
+//     />
+//   );
+// }
