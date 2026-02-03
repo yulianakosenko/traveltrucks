a21edb9e7bf80b8b03f5a5a2cfe9843dce9d708a
YULIYA_KOSTENKO <yuliashkodina1@gmail.com>
2026-02-03 20:49:23 +0100

    feat: add load more functionality to catalog page

--- src/pages/CatalogPage.jsx
+++ src/pages/CatalogPage.jsx
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchCampers } from "../redux/campersSlice";
 
 import CamperCard from "../components/CamperCard/CamperCard";
 import Filters from "../components/Filters/Filters";
 
+const PAGE_SIZE = 4;
+
 export default function CatalogPage() {
   const dispatch = useDispatch();
-
   const { items, isLoading } = useSelector((state) => state.campers);
+
+  const [visibleCount, setVisibleCount] = useState(PAGE_SIZE);
 
   useEffect(() => {
     dispatch(fetchCampers());
   }, [dispatch]);
+
+  const visibleItems = items.slice(0, visibleCount);
 
   return (
     <section className="catalogPage">
@@ -29,10 +34,21 @@ export default function CatalogPage() {
           {!isLoading && items.length === 0 && <p>No campers found</p>}
 
           <div className="catalogCards">
-            {items.map((camper) => (
+            {visibleItems.map((camper) => (
               <CamperCard key={camper.id} camper={camper} />
             ))}
           </div>
+
+          {/* LOAD MORE */}
+          {!isLoading && visibleCount < items.length && (
+            <button
+              type="button"
+              className="loadMore"
+              onClick={() => setVisibleCount((prev) => prev + PAGE_SIZE)}
+            >
+              Load more
+            </button>
+          )}
         </div>
       </div>
     </section>
